d9372cb782d338279e5ca810ba64d1dc88b5a532
MiguelAValdesG <mvaldes988@gmail.com>
2021-08-26 13:24:31 -0500

    'Java If-Else' exercise from HackerRank done

---
+++ MiguelAValdesG/Hackerrank/JavaIfElse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class JavaIfElse {
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int N = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    if (N >= 1 && N <= 100) {
+      if (N % 2 == 1 || (N >= 6 && N <= 20))
+        System.out.println("Weird");
+      else
+        System.out.println("Not Weird");
+    } else
+      System.out.println("The number have be between 1 and 100");
+
+    scanner.close();
+  }
+}
