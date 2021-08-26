5aabffdbe80f2c058f7b0c5c76e9d2d2fb3fd7dd
David Gonzalez <davidgzu@gmail.com>
2021-08-26 00:04:38 -0500

    [Add] HackerRank exercise solutions

---
+++ DavidGonzalezZumaya/Day2/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main ( String args[] ) {
+
+        SolutionA solutionA = new SolutionA();
+        solutionA.Scanner();
+
+        SolutionB solutionB = new SolutionB();
+        solutionB.solve();
+
+        SolutionC solutionC = new SolutionC();
+        solutionC.solve();
+
+
+    }
+}
