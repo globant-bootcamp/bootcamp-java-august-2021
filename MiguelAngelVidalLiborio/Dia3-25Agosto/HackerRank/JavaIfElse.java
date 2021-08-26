0beaac190d4af75c48263906950c2a3ca14f336a
miguel angel <mikeangelvl1097@gmail.com>
2021-08-25 22:58:49 -0500

    crear carpetas, pasar lista y agregar los ejercicios de hackerrank -miguelangel

---
+++ MiguelAngelVidalLiborio/Dia3-25Agosto/HackerRank/JavaIfElse.java
@@ -0,0 +1,27 @@
+package logia;
+
+import java.util.Scanner;
+public class JavaIfElse {
+
+	public static void main(String[] args) {
+
+		Scanner scanner=new Scanner(System.in);
+		int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if(N % 2 ==0 && N > 20){
+            System.out.println("Not Weird");   
+        }
+        if(N % 2==1){
+            System.out.println("Weird"); 
+        }
+        if(N % 2 ==0 && (N >=2 && N <=5) ){
+            System.out.println("Not Weird");   
+        }
+        if(N % 2 ==0 && (N >=6 && N <=20) ){
+            System.out.println("Weird"); 
+        }
+        scanner.close();
+	}
+
+}
---
+++ MiguelAngelVidalLiborio/Dia3-25Agosto/HackerRank/JavaStdinandStdout2.java
@@ -0,0 +1,19 @@
+package logia;
+import java.util.*;
+public class JavaStdinandStdout2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        String s=scan.nextLine();
+
+        // Write your code here.
+        s=scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+	}
+
+}
