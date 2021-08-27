3f5d3b54c0325de950c65b41879b2180f46ae16e
Dfloresa <55201763+Dfloresa@users.noreply.github.com>
2021-08-27 16:00:00 -0500

    Update if-else.java

--- DiegoFlores/if-else.java
+++ DiegoFlores/if-else.java
@@ -13,20 +13,14 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int N = scanner.nextInt();
+        int number = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-
-        if(N%2 == 1){
+        if(number%2 == 1 || ( number > 6 && number <= 20 )){
             System.out.println("Weird");
-        }else if( N > 2 && N < 5){
-            System.out.println("Not Weird");
-        }else if (N > 6 && N <= 20){
-            System.out.println("Weird");
-        }else if ( N > 20){
+        }else if( number > 2 && number < 5  || (number > 20)){
             System.out.println("Not Weird");
         }
-
 
         scanner.close();
         
