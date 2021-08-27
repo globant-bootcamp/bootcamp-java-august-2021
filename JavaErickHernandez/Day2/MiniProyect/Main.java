349aa5d2c8534ae9fa6f611b3bcff1d1806cbf10
1699kratos <erickhdzrdz@hotmail.com>
2021-08-27 17:06:25 -0500

    Added more polymorphism

--- JavaErickHernandez/Day2/MiniProyect/Main.java
+++ JavaErickHernandez/Day2/MiniProyect/Main.java
@@ -1,17 +1,26 @@
 package bootcamp;
 public class Main {
 	public static void main(String[] Args) {
-		Sedan sedan = new Sedan("Nissan", "Sentra", "Standard", 2019, 210, true);
-		String messageSedan = sedan.dataVehicle();
-		String gasSedan = sedan.gasType("diesel");
-		System.out.print(messageSedan);
-		System.out.print(gasSedan);
-		
-		
-		Truck truck = new Truck("Ford", "Windstar", "Automatic", 2000, 60, "2 Wheels", 8);
-		String messageTruck = truck.dataVehicle();
-		String gasTruck = truck.gasType("premium");
-		System.out.print(messageTruck);
-		System.out.print(gasTruck);
+		Vehicle v1 = new Sedan("Nissan", "Sentra", "Standard", 2019, 210, true);
+		Vehicle v2 = new Truck("Ford", "Windstar", "Automatic", 2000, 60, "2 Wheels", 8);
+		defineVehicle(v1);
+		defineVehicle(v2);
+	}	
+	public static void defineVehicle(Vehicle vehicle) {
+		if(vehicle instanceof Sedan) {
+			String messageSedan = vehicle.dataVehicle();
+			System.out.print(messageSedan);
+			String gasSedan = ((Sedan) vehicle).gasType("diesel");
+			System.out.print(gasSedan);
+			System.out.println("");
+		}else if(vehicle instanceof Truck) {
+			String messageTruck = vehicle.dataVehicle();
+			System.out.print(messageTruck);
+			String gasTruck = ((Truck) vehicle).gasType("premium");
+			System.out.print(gasTruck);
+			System.out.println("");
+		}else {
+			System.out.println("Sorry we don´t count with that vehicle");
+		}
 	}
 }
