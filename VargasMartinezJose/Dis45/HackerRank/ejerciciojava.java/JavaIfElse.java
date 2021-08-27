f4614f40c752f6be541205bc704568459a31caab
Jose <josevargas@MacBook-Air-de-Jose.local>
2021-08-27 02:37:14 -0500

    EjerciciosHackerRank

---
+++ VargasMartinezJose/Dis45/HackerRank/ejerciciojava.java/JavaIfElse.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class JavaIfElse{
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        if(N % 2 ==1){
+            System.out.println("Weird");
+        }
+        else if (N % 2 ==0 & N >= 2 && N <= 5){
+            System.out.println("Not Weird");
+        }
+        
+        else if (N % 2 ==0 & N >= 6 && N <= 20){
+             System.out.println("Weird");
+        }
+        else if (N % 2 ==0 & (N > 20 & N <=100)){
+            System.out.println("Not Weird");
+        }
+        else {
+            System.out.println("Number greater 100");
+        }
+         
+        scanner.close();
+    }
+}
---
+++ VargasMartinezJose/Dis45/HackerRank/ejerciciojava.java/JavaStdinStdout1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class JavaStdinStdout1{
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+       
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        scan.close();
+    }
+}
+
