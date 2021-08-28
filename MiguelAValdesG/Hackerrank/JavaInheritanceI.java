21839de59a9af0c20cf2e17d1ca19181bf4c1a63
MiguelAValdesG <mvaldes988@gmail.com>
2021-08-27 23:42:20 -0500

    'Java Inheritance I' excercise from HackerRank done

---
+++ MiguelAValdesG/Hackerrank/JavaInheritanceI.java
@@ -0,0 +1,28 @@
+class Animal{
+  void walk() {
+    final String WALKING = "I am walking";
+    System.out.println(WALKING);
+  }
+}
+
+class Bird extends Animal {
+  void fly() {
+    final String FLYING = "I am flying";
+    System.out.println(FLYING);
+  }
+
+  void sing() {
+    final String SINGING = "I am singing";
+    System.out.println(SINGING);
+  }
+}
+
+public class JavaInheritanceI{
+
+    public static void main(String[] args) {
+      Bird bird = new Bird();
+      bird.walk();
+      bird.fly();
+      bird.sing();
+    }
+}
