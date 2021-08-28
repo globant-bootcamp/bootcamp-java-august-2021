0c8c9ea06b239c584fbd5380750176e403ad6ad6
Williiam8 <jesusgarca01@gmail.com>
2021-08-28 13:17:37 -0500

    Magic String

--- WilliamsGarcia/26-08/HackerRank/OutputFormatting/OutputFormatting.java
+++ WilliamsGarcia/26-08/HackerRank/OutputFormatting/OutputFormatting.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class OutputFormatting {
 
+    public static final String SEPARATOR = "================================";
+    public static final int CYCLE_NUMBER = 3;
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
-        System.out.println("================================");
-        for (int i = 0; i < 3; i++) {
+        System.out.println(SEPARATOR);
+        for (int i = 0; i < CYCLE_NUMBER; i++) {
             String input = scan.next();
             int auxInput = scan.nextInt();
             System.out.printf("%-15s%03d%n", input, auxInput);
         }
-        System.out.println("================================");
+        System.out.println(SEPARATOR);
 
     }
 }
