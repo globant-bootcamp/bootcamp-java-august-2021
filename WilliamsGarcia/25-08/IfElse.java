d6974aaf3992758684049b3f9c2adffde5823da5
Williiam8 <jesusgarca01@gmail.com>
2021-08-27 20:35:57 -0500

    Magic String V2

--- WilliamsGarcia/25-08/IfElse.java
+++ WilliamsGarcia/25-08/IfElse.java
@@ -4,20 +4,23 @@ import java.util.Scanner;
 
 public class IfElse {
 
+    public static final Scanner SCANNER = new Scanner(System.in);
+    public static final String WEIRD = "Weird";
+    public static final String NOT_WEIRD = "Weird";
+
     public static void main(String[] args) {
 
-        final Scanner SCANNER = new Scanner(System.in);
         int inputNumber = SCANNER.nextInt();
 
         if (inputNumber <= 100 && inputNumber >= 1) {
             if (inputNumber % 2 == 0) {
                 if ((inputNumber >= 2 && inputNumber <= 5) || inputNumber > 20) {
-                    System.out.println("Not Weird");
+                    System.out.println(NOT_WEIRD);
                 } else {
-                    System.out.println("Weird");
+                    System.out.println(WEIRD);
                 }
             } else {
-                System.out.println("Weird");
+                System.out.println(WEIRD);
             }
         }
 
--- WilliamsGarcia/25-08/StdIntStdOutOne.java
+++ WilliamsGarcia/25-08/StdIntStdOutOne.java
@@ -3,11 +3,13 @@ package com.company;
 import java.util.Scanner;
 
 public class StdIntStdOutOne {
-    
+
+    public static final int CYCLE_NUMBER = 3;
+
     public static void main(String[] args) {
         int cont = 0;
         Scanner scan = new Scanner(System.in);
-        while (cont < 3) {
+        while (cont < CYCLE_NUMBER) {
             cont++;
             System.out.println(scan.nextInt());
         }
