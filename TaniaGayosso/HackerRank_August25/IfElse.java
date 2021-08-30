ea885339636588b28efeb8e5604c03a2e74855d1
Tania Gayosso <taniagd09@gmail.com>
2021-08-30 17:00:16 -0500

    Exercise IfElse fixed

--- TaniaGayosso/HackerRank_August25/IfElse.java
+++ TaniaGayosso/HackerRank_August25/IfElse.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 
 public class IfElse {
 
-	private static final Scanner scanner = new Scanner(System.in);
+	public static final Scanner SCANNER = new Scanner(System.in);
 	public static final String WEIRD_NUMBER = "Weird";
-	public static final String NOT_WEIRD_NUMBER = "Not Weird"; 
-	
+	public static final String NOT_WEIRD_NUMBER = "Not Weird";
+
 	public static void main(String[] args) {
 
-		int number = scanner.nextInt();
-		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		int number = SCANNER.nextInt();
 
-		if (number % 2 == 1 || number >= 6 && number <= 20)  {
-			System.out.println (WEIRD_NUMBER);
-		} else {
-			System.out.println (NOT_WEIRD_NUMBER);
+		if (number % 2 == 0) {
+			if ((number >= 2 && number <= 5) || number > 20) {
+				System.out.println(NOT_WEIRD_NUMBER);
+			} else {
+				System.out.println(WEIRD_NUMBER);
 			}
-		scanner.close();
+		} else {
+			System.out.println(WEIRD_NUMBER);
 		}
+
+		SCANNER.close();
+	}
 }
