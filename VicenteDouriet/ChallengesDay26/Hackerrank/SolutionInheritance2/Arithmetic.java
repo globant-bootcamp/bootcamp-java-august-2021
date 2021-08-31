2d1c744e9f445177777fe3d14bf68d1ff831e1be
LawlietLelouch03 <pachi_lord@hotmail.com>
2021-08-30 23:47:33 -0500

    updating variable name

--- VicenteDouriet/ChallengesDay26/Hackerrank/SolutionInheritance2/Arithmetic.java
+++ VicenteDouriet/ChallengesDay26/Hackerrank/SolutionInheritance2/Arithmetic.java
@@ -1,7 +1,7 @@
 package com.king_arthur.hackerrank.inheritance2;
 
 class Arithmetic {
-    int add(int a, int b){
-        return a+b;
+    int add(int numOne, int numTwo){
+        return numOne+numTwo;
     }
 }
