5bfec374cf370fbb74efc8bf88c6860381555152
Jose <josevargas@MacBook-Air-de-Jose.local>
2021-08-30 23:41:25 -0500

    Correciones HR Dia 2

--- VargasMartinezJose/Dis45/HackerRank/ejerciciojava.java/JavaIfElse.java
+++ VargasMartinezJose/Dis45/HackerRank/ejerciciojava.java/JavaIfElse.java
@@ -11,12 +11,10 @@ public class JavaIfElse {
         int number = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        if (number % 2 == 1 || (number % 2 == 0) & (number >= 6 & number <= 20)) {
+        if (number % 2 == 1 || number >= 6 & number <= 20) {
             System.out.println("Weird");
-        } else if (number % 2 == 0 & (number > 20 && number <= 100 || number >= 2 & number <= 5)) {
+        } else if (number > 20 || number >= 2 & number <= 5) {
             System.out.println("Not Weird");
-        } else {
-            System.out.println("Number greater 100");
         }
 
         scanner.close();
