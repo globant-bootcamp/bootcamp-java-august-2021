83f7557548a64f798bdbd7bf2afa4512622fc70e
Jose <josevargas@MacBook-Air-de-Jose.local>
2021-09-01 01:11:09 -0500

    Correciones HR Dia 3

--- VargasMartinezJose/Dia3/HackerRank/Bird.java
+++ VargasMartinezJose/Dia3/HackerRank/Bird.java
@@ -1,15 +1,12 @@
 package bootcamp.java.day.three.bd;
 
 class Bird extends Animal {
-
     void fly() {
         System.out.println("I am flying");
     }
-
     void walk() {
         System.out.println("I am walking");
     }
-
     void sing() {
         System.out.println("I am singing");
     }
--- VargasMartinezJose/Dia3/HackerRank/MainAnimal.java
+++ VargasMartinezJose/Dia3/HackerRank/MainAnimal.java
@@ -1,13 +1,10 @@
 package bootcamp.java.day.three.ma;
 
 public class MainAnimal {
-
    public static void main(String args[]) {
-
       Bird bird = new Bird();
       bird.walk();
       bird.fly();
       bird.sing();
-
    }
 }
