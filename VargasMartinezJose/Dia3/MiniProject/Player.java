80618dd86771d8929f87ce36142ba64417eaf0a6
Jose <josevargas@MacBook-Air-de-Jose.local>
2021-09-01 01:07:07 -0500

    Correciones MP Dia 3

--- VargasMartinezJose/Dia3/MiniProject/Player.java
+++ VargasMartinezJose/Dia3/MiniProject/Player.java
@@ -1,10 +1,9 @@
 package miniproject.bootcamp.np;
 
-public class NewPlayer {
-
-    public static final String NULL_VALUE = "ENTER DATA";
-    public static final int NULL_INT = 0;
-    String[] strArray = new String[] { "ID: ", "NAME: ", "AGE: ", "STATUS: ", "TEAM: ", "POSITION: ", "RECORD: " };
+public class Player{
+    public static final String INVALID_DATA = "ENTER DATA";
+    public static final int INVALID_VALUE = 0;
+    String[] playerLabels = new String[] { "ID: ", "NAME: ", "AGE: ", "STATUS: ", "TEAM: ", "POSITION: ", "RECORD: " };
     public int id;
     public String name;
     public int age;
@@ -13,7 +12,7 @@ public class NewPlayer {
     public String position;
     public int record;
 
-    public NewPlayer(int id, String name, int age, String status, String team, String position, int record) {
+    public Player(int id, String name, int age, String status, String team, String position, int record) {
         this.id = id;
         this.name = name;
         this.age = age;
@@ -21,88 +20,73 @@ public class NewPlayer {
         this.team = team;
         this.position = position;
         this.record = record;
-
     }
 
-    public NewPlayer(int id, String name) {
+    public Player(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public NewPlayer() {
-        this.id = NULL_INT;
-        this.name = NULL_VALUE;
-        this.age = NULL_INT;
-        this.status = NULL_VALUE;
-        this.team = NULL_VALUE;
-        this.position = NULL_VALUE;
-        this.record = NULL_INT;
+    public Player() {
+        this.id = INVALID_VALUE;
+        this.name = INVALID_DATA;
+        this.age = INVALID_VALUE;
+        this.status = INVALID_DATA;
+        this.team = INVALID_DATA;
+        this.position = INVALID_DATA;
+        this.record = INVALID_VALUE;
     }
-
+    
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
-    }
-
+    }   
     public void setAge(int age) {
         this.age = age;
     }
-
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
-
     public String getTeam() {
         return team;
     }
-
     public void setTeam(String team) {
         this.team = team;
     }
-
     public String getPosition() {
         return position;
     }
-
     public void setPosition(String position) {
         this.position = position;
     }
-
     public int getRecord() {
         return record;
     }
-
     public void setRecord(int record) {
         this.record = record;
     }
-
     public void showData(String className) {
         System.out.println(className);
-        System.out.println(strArray[0] + id);
-        System.out.println(strArray[1] + name);
-        System.out.println(strArray[2] + age);
-        System.out.println(strArray[3] + status);
-        System.out.println(strArray[4] + team);
-        System.out.println(strArray[5] + position);
-        System.out.println(strArray[6] + record);
+        System.out.println(playerLabels[0] + id);
+        System.out.println(playerLabels[1] + name);
+        System.out.println(playerLabels[2] + age);
+        System.out.println(playerLabels[3] + status);
+        System.out.println(playerLabels[4] + team);
+        System.out.println(playerLabels[5] + position);
+        System.out.println(playerLabels[6] + record);
     }
 }
--- VargasMartinezJose/Dia3/MiniProject/TransferablePlayer.java
+++ VargasMartinezJose/Dia3/MiniProject/TransferablePlayer.java
@@ -1,38 +1,29 @@
 package miniproject.bootcamp.tf;
 
-public class TransferablePl extends NewPlayer {
-
-    String[] strArray2 = new String[] { "VALUE: ", "NEW TEAM: ", "INJURIES: " };
-
-    public TransferablePl(int id, String name, int age, String status, String team, String position, int record,
+public class TransferablePlayer extends Player {
+    String[] transferableLabels = new String[] { "VALUE: ", "NEW TEAM: ", "INJURIES: " };
+    public TransferablePlayer(int id, String name, int age, String status, String team, String position, int record,
             int value, String newTeam, int injuries) {
         super(id, name, age, status, team, position, record);
         this.value = value;
         this.newTeam = newTeam;
         this.injuries = injuries;
-
     }
-
-    public TransferablePl(int id, String name) {
+    public TransferablePlayer(int id, String name) {
         this.id = id;
         this.name = name;
-
     }
-
-    public TransferablePl() {
-
-        this.id = NULL_INT;
-        this.name = NULL_VALUE;
-        this.age = NULL_INT;
-        this.status = NULL_VALUE;
-        this.position = NULL_VALUE;
-        this.record = NULL_INT;
-        this.value = NULL_INT;
-        this.newTeam = NULL_VALUE;
-        this.injuries = NULL_INT;
-
+    public TransferablePlayer() {
+        this.id = INVALID_VALUE;
+        this.name =INVALID_DATA;
+        this.age = INVALID_VALUE;
+        this.status = INVALID_DATA;
+        this.position =INVALID_DATA;
+        this.record = INVALID_VALUE;
+        this.value = INVALID_VALUE;
+        this.newTeam = INVALID_DATA;
+        this.injuries = INVALID_VALUE;
     }
-
     private int value;
     private String newTeam;
     private int injuries;
@@ -40,33 +31,26 @@ public class TransferablePl extends NewPlayer {
     public int getValue() {
         return value;
     }
-
     public void setValue(int value) {
         this.value = value;
     }
-
     public String getNewTeam() {
         return newTeam;
     }
-
     public void setNewTeam(String newTeam) {
         this.newTeam = newTeam;
     }
-
-    public int getInjuries() {
+     public int getInjuries() {
         return injuries;
     }
-
     public void setInjuries(int injuries) {
         this.injuries = injuries;
     }
-
     @Override
     public void showData(String className) {
         super.showData(className);
-        System.out.println(strArray2[0] + value);
-        System.out.println(strArray2[1] + newTeam);
-        System.out.println(strArray2[2] + injuries);
+        System.out.println(transferableLabels[0] + value);
+        System.out.println(transferableLabels[1] + newTeam);
+        System.out.println(transferableLabels[2] + injuries);
     }
-
 }
