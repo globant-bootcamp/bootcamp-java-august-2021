59163ae1858a44d545b895911a326a2b92909837
ClaudiaHG23 <claudia.hernandezgarcia@hotmail.com>
2021-09-01 01:07:10 -0500

    adding camelcase in variables

--- ClaudiaHernandezGarcia/30Aug/HackerRank/javaHashset/Solution.java
+++ ClaudiaHernandezGarcia/30Aug/HackerRank/javaHashset/Solution.java
@@ -7,18 +7,18 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfStringPairs = scanner.nextInt();
-        String[] pair_left = new String[numberOfStringPairs];
-        String[] pair_right = new String[numberOfStringPairs];
+        String[] pairLeft = new String[numberOfStringPairs];
+        String[] pairRight = new String[numberOfStringPairs];
 
         for (int i = 0; i < numberOfStringPairs; i++) {
-            pair_left[i] = scanner.next();
-            pair_right[i] = scanner.next();
+            pairLeft[i] = scanner.next();
+            pairRight[i] = scanner.next();
         }
 
         HashSet<String> hashSet = new HashSet<String>();
         
         for (int i = 0; i < numberOfStringPairs; i++) {
-            hashSet.add(pair_left[i] + " " + pair_right[i]);
+            hashSet.add(pairLeft[i] + " " + pairRight[i]);
             System.out.println(hashSet);
             System.out.println(hashSet.size());
         }
