575d95d5dc9ba7cf759343a7e276252ac73e3dcf
MiguelAValdesG <mvaldes988@gmail.com>
2021-09-02 00:00:37 -0500

    'Java Hashset' exercise from HackerRank done

---
+++ MiguelAValdesG/Hackerrank/JavaHashSet.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class JavaHashSet {
+
+  public static void main(String[] args) {
+    Scanner readLineTerminal = new Scanner(System.in);
+    HashSet<String> stringPairs = new HashSet<>();
+    final String SPACE = " ";
+
+    int numberOfElements = readLineTerminal.nextInt();
+    readLineTerminal.nextLine();
+
+    String[] pairLeft = new String[numberOfElements];
+    String[] pairRight = new String[numberOfElements];
+
+    for (int index = 0; index < numberOfElements; index++) {
+      pairLeft[index] = readLineTerminal.nextLine();
+      pairRight[index] = readLineTerminal.nextLine();
+    }
+
+    for (int index = 0; index < numberOfElements; index++) {
+      stringPairs.add(pairLeft[index] + SPACE + pairRight[index]);
+      System.out.println(stringPairs.size());
+    }
+  }
+}
+
+// Challenge: https://www.hackerrank.com/challenges/java-hashset/problem
