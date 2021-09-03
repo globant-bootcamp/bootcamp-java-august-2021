ac4cab536dcc141abb81073112dc5f26430557a3
jesus-luna <chuy_braulio@hotmail.com>
2021-09-03 01:06:26 -0500

    Creating student list with constraint

--- jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/hackerrank/august30/sort/ReArrange.java
+++ jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/hackerrank/august30/sort/ReArrange.java
@@ -1,6 +1,8 @@
 package bootcamp.hackerrank.august30.sort;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 
 import bootcamp.hackerrank.Solution;
 
@@ -8,8 +10,12 @@ public class ReArrange extends Solution {
 
 	private static final String NUMBER_STUDENTS_INPUT_INSTRUCTIONS = "Give the number of students: ";
 	private static final String NUMBER_STUDENTS_INPUT_INVALID = "The expected value is a number between 2 - 1000";
-	private static final String NUMBER_ID_INPUT_INSTRUCTIONS = "Give the ID of the student : ";
-	private static final String NUMBER_ID_INPUT_INVALID = "The expected value is a number between 0 - 10000";
+	private static final String ID_INPUT_INSTRUCTIONS = "Give the ID of the student : ";
+	private static final String ID_INPUT_INVALID = "The expected value is a number between 0 - 10000";
+	private static final String CGPA_INPUT_INSTRUCTIONS = "Give the CGPA of the student : ";
+	private static final String CGPA_INPUT_INVALID = "The expected value is a number between 0.00 - 4.00 and will contain, at most, 2 digits after the decimal point.";
+	private static final String NAME_INPUT_INSTRUCTIONS = "Give the name of the student : ";
+	private static final String NAME_INPUT_INVALID = "The expected value is a string with a length between 5 - 30";
 
 	@Override
 	public void solve() {
@@ -17,25 +23,28 @@ public class ReArrange extends Solution {
 	}
 
 	private void createStudentsList() {
-		boolean validValue = true;
 		int numberOfStudents;
 		int idStudent;
-		numberOfStudents = readNumberOfStudentsIteration();
-		
+		double cgpa;
+		String name;
+		numberOfStudents = readNumberOfStudentsRecursive();
+		List<Student> listOfStudents = new ArrayList<>();
 		for (int index = 0; index < numberOfStudents; index++) {
-			
+			idStudent = readIdStudent();
+			cgpa = readCgpa();
+			name = readName();
+			listOfStudents.add(new Student(idStudent, name, cgpa));
 		}
+		listOfStudents.forEach(s->System.out.println(s.toString()));
 	}
 	
 	private int readNumberOfStudentsIteration() {
 		boolean validValue = true;
 		int numberOfStudents;		
 		do {
-				numberOfStudents = readNumberInput(ReArrange.NUMBER_STUDENTS_INPUT_INSTRUCTIONS);
+				numberOfStudents = readIntegerInput(ReArrange.NUMBER_STUDENTS_INPUT_INSTRUCTIONS);
 				validValue = true;
-				System.out.println("The value introduced is "+numberOfStudents);
 				if(numberOfStudents < 2 || numberOfStudents > 10000) {
-					System.out.println("inside here");
 					System.out.println(ReArrange.NUMBER_STUDENTS_INPUT_INVALID);
 					validValue = false;
 				}
@@ -45,26 +54,71 @@ public class ReArrange extends Solution {
 	
 	private int readNumberOfStudentsRecursive() {
 		int numberOfStudents;
-		numberOfStudents = readNumberInput(ReArrange.NUMBER_STUDENTS_INPUT_INSTRUCTIONS);
+		numberOfStudents = readIntegerInput(ReArrange.NUMBER_STUDENTS_INPUT_INSTRUCTIONS);
 		if(numberOfStudents < 2 || numberOfStudents > 10000) {
 			System.out.println(ReArrange.NUMBER_STUDENTS_INPUT_INVALID);
 			numberOfStudents = readNumberOfStudentsRecursive();
 		}
 		return numberOfStudents;
 	}
-
-	private int readNumberInput(String instruction) {
+	
+	private int readIdStudent() {
+		int idStudent;
+		idStudent = readIntegerInput(ReArrange.ID_INPUT_INSTRUCTIONS);
+		if(idStudent < 0 || idStudent > 100000) {
+			System.out.println(ReArrange.ID_INPUT_INVALID);
+			idStudent = readIdStudent();
+		}
+		return idStudent;
+	}
+	
+	private double readCgpa() {
+		double cgpa;
+		cgpa = readDoubleInput(ReArrange.CGPA_INPUT_INSTRUCTIONS);
+		boolean cgpaFormat = String.valueOf(cgpa).matches("([0-3]{1}\\.[0-9]{2,}|4.00)");
+		if(cgpa < 0 || cgpa > 4.00 | !cgpaFormat) {
+			System.out.println(ReArrange.CGPA_INPUT_INVALID);
+			cgpa = readCgpa();
+		}
+		return cgpa;
+	}
+	
+	private String readName() {
+		String name;
+		System.out.println(ReArrange.NAME_INPUT_INSTRUCTIONS);
+		Solution.scanner.nextLine();
+		name = Solution.scanner.nextLine().toLowerCase();
+		if(name.length() < 5 || name.length() > 30) {
+			System.out.println(ReArrange.NAME_INPUT_INVALID);
+			
+			name = readName();
+		}
+		return name;
+	}
+	
+	private int readIntegerInput(String instruction) {
 		int intInput;
 		try {
 			System.out.print(instruction);
 			intInput = Solution.scanner.nextInt();
 		} catch (InputMismatchException e) {
 			Solution.scanner.nextLine();
-			intInput = readNumberInput(instruction);
+			intInput = readIntegerInput(instruction);
 		}
 		return intInput;
 	}
 	
+	private double readDoubleInput(String instruction) {
+		double doubleInput;
+		try {
+			System.out.print(instruction);
+			doubleInput = Solution.scanner.nextDouble();
+		} catch (InputMismatchException e) {
+			Solution.scanner.nextLine();
+			doubleInput = readDoubleInput(instruction);
+		}
+		return doubleInput;
+	}
 	
 
 }
