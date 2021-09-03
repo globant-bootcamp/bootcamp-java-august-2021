c6723352219c4401030fd34389e2f78707f4bd2d
jesus-luna <chuy_braulio@hotmail.com>
2021-09-03 01:06:48 -0500

    changing attribute

--- jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/hackerrank/august30/sort/Student.java
+++ jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/hackerrank/august30/sort/Student.java
@@ -3,15 +3,15 @@ package bootcamp.hackerrank.august30.sort;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 @Getter
+@ToString
 public class Student {
 	private int id;
-	private String fname;
+	private String name;
 	private double cgpa;
+	
 }
