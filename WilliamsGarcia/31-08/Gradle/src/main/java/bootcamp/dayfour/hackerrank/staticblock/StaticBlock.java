ff6eea3dd80de9747c52f7744a2723a5046c9ab7
Williiam8 <jesusgarca01@gmail.com>
2021-09-03 09:56:20 -0500

    Hackerrank with gradle & Log4j2

---
+++ WilliamsGarcia/31-08/Gradle/src/main/java/bootcamp/dayfour/hackerrank/staticblock/StaticBlock.java
@@ -0,0 +1,35 @@
+package bootcamp.dayfour.hackerrank.staticblock;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+public class StaticBlock {
+
+    static Scanner scan = new Scanner(System.in);
+    static int inputNumberOne = scan.nextInt();
+    static int inputNumberTwo = scan.nextInt();
+    static final Logger LOGGER = LogManager.getLogger(StaticBlock.class);
+    static final String ERROR = "java.lang.Exception: Breadth and height must be positive";
+    static boolean flag = isValidInput();
+
+    private static boolean isValidInput() {
+        boolean auxiliaryFlag;
+        if (inputNumberOne > 0 && inputNumberTwo > 0) {
+            auxiliaryFlag = true;
+        } else {
+            auxiliaryFlag = false;
+            LOGGER.error(ERROR);
+        }
+        return auxiliaryFlag;
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = inputNumberOne * inputNumberTwo;
+            LOGGER.info(area);
+        }
+
+    }
+}
