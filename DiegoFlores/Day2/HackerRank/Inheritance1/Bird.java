c956215d61bf171377a9af1694440c76d4fe4886
Diego Flores Alemán <55201763+Dfloresa@users.noreply.github.com>
2021-09-04 15:04:43 -0500

    Update Bird.java
    Created final static variables "FLYING_MESSAGE" and "SINGING_MESSAGE"

--- DiegoFlores/Day2/HackerRank/Inheritance1/Bird.java
+++ DiegoFlores/Day2/HackerRank/Inheritance1/Bird.java
@@ -1,13 +1,16 @@
 package animal;
 
+public static final String FLYING_MESSAGE = "I am flying";
+public static final String SINGING_MESSAGE = "I am singing";
+
 public class Bird extends Animal{
-	
-	void fly(){
-		System.out.println("I am flying");
-	}
+
+    void fly(){
+	System.out.println(FLYING_MESSAGE);
+    }
     
     void sing(){
-        System.out.println("I am singing");
+        System.out.println(SINGING_MESSAGE);
     }
     
 }
