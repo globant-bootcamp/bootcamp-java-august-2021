df16dfd6569935b99a78c686f46553d6d9e0d8b9
Diego Flores Alemán <55201763+Dfloresa@users.noreply.github.com>
2021-09-04 15:23:31 -0500

    Update IntToString.java
    indentation was fix.
    Static final variables created.

--- DiegoFlores/Gradlee/src/main/java/hackerrank/IntToString.java
+++ DiegoFlores/Gradlee/src/main/java/hackerrank/IntToString.java
@@ -2,28 +2,30 @@ package hackerrank;
 
 import java.util.Scanner;
 
+public static final String GOOD_JOB  = "Good job";
+public static final String WRONG_ANSWER = "Wrong answer.";
+public static final String ERROR = "Unsuccessful Termination!!";
+
 public class IntToString {
 	
 	public static void main(String[] args) {
-
-	
-		  try {
-		   Scanner in = new Scanner(System.in);
-		   int inputNumber = in .nextInt();
-		   in.close();
-		 
-		    String stringNumber = Integer.toString(inputNumber);
+		
+		try {
 		   
-		   if (inputNumber == Integer.parseInt(stringNumber)) {
-		    System.out.println("Good job");
-		   } else {
-		    System.out.println("Wrong answer.");
-		   }
-		  } catch (Exception e) {
-		   System.out.println("Unsuccessful Termination!!");
-		  }
-		 }
-	
-	
+			Scanner in = new Scanner(System.in);
+		   	int inputNumber = in .nextInt();
+		   	in.close();		 
+		   	String stringNumber = Integer.toString(inputNumber);
+		   
+		   	if (inputNumber == Integer.parseInt(stringNumber)) {
+		    		System.out.println(GOOD_JOB);
+		   	} else {
+		    		System.out.println(WRONG_ANSWER);
+		  	}
+		
+		    } catch (Exception e) {
+		   	System.out.println(ERROR);
+		    }
+	}
 	
 }
