882070036fe4e712bc10949a0f2397742562119a
Diego Flores Alemán <55201763+Dfloresa@users.noreply.github.com>
2021-09-04 15:17:02 -0500

    Update Map.java
    Indentation fixed.

--- DiegoFlores/Day3/Map.java
+++ DiegoFlores/Day3/Map.java
@@ -7,38 +7,36 @@ class Solution{
     
     public static final String ERROMESSAGE = "Not found";
     
-	public static void main(String []args)
-	{
-        Map<String, Integer> phoneBook = new HashMap<String, Integer>();  
+	public static void main(String []args){
+		
+        	Map<String, Integer> phoneBook = new HashMap<String, Integer>();  
             
 		Scanner in = new Scanner(System.in);
 		int numberEntries = in.nextInt();    
-        in.nextLine();
+        	in.nextLine();
         
-		for(int i=0;i<numberEntries;i++)
-		{
+		for(int i=0;i<numberEntries;i++){
+			
 			String name=in.nextLine();
-			int phoneNumber = in.nextInt();
-            
-            phoneBook.put(name, phoneNumber);
-            
+			int phoneNumber = in.nextInt();            
+            		phoneBook.put(name, phoneNumber);            
 			in.nextLine();
+			
 		}
         
-		while(in.hasNext())
-		{
+		while(in.hasNext()){
+			
 			String name = in.nextLine();
-            Integer phoneNumber = phoneBook.get(name);
+            		Integer phoneNumber = phoneBook.get(name);
             
-            if( phoneNumber == null){
-                System.out.println(ERROMESSAGE);
-            }else{
-                System.out.println( name + "=" + phoneNumber);
-            }
+            		if( phoneNumber == null){
+                		System.out.println(ERROMESSAGE);
+            		}else{
+                		System.out.println( name + "=" + phoneNumber);
+           		}
+			
 		}
         
 	}
+	
 }
-
-
-
