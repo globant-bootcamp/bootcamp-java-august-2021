2dd291a8daf1af49a5451071d43786a6d523dbf3
Diego Flores Alemán <55201763+Dfloresa@users.noreply.github.com>
2021-09-04 14:28:33 -0500

    Update Main.java
    Added separation by "_"  in all final variables.
    The final variables are used now.
    Name changed in "set" and "get" methods due to changes in "person" class (now named "User")

--- DiegoFlores/Day2/Challenge/Main.java
+++ DiegoFlores/Day2/Challenge/Main.java
@@ -1,45 +1,45 @@
 package person;
 
-    	public static final String STUDENT1CAREER = "architecture";
-	public static final String STUDENT1NAME = "John";
-	public static final String STUDENT1SURNAME = "Connor";
+    	public static final String STUDENT_ONE_CAREER = "architecture";
+	public static final String STUDENT_ONE_NAME = "John";
+	public static final String STUDENT_ONE_LASTNAME = "Connor";
 
-	public static final String STUDENT2CAREER = "Biology";
-	public static final String STUDENT2NAME = "Anakin";
-	public static final String STUDENT2SURNAME = "Skywalker";
+	public static final String STUDENT_TWO_AREER = "Biology";
+	public static final String STUDENT_TWO_NAME = "Anakin";
+	public static final String STUDENT_TWO_LASTNAME = "Skywalker";
 
-	public static final String TEACHER1COURSE  = "Structures";
-	public static final String TEACHER1TIME = "Full time";
-	public static final String TEACHER1NAME = "Obi-Wan";
-	public static final String TEACHER1SURNAME = "Kenobi";
+	public static final String TEACHER_ONE_COURSE  = "Structures";
+	public static final String TEACHER_ONE_TIME = "Full time";
+	public static final String TEACHER_ONE_NAME = "Obi-Wan";
+	public static final String TEACHER_ONE_SURNAME = "Kenobi";
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Student student1 = new Student(000001,"architecture",1);
-			student1.setNames("John");
-			student1.setSurnames("Connor");
-			student1.setAge(12);
+		Student student1 = new Student(000001,STUDENT_ONE_CAREER,1);
+			student1.setUserNames(STUDENT_ONE_NAME);
+			student1.setUserLastName(STUDENT_ONE_LASTNAME);
+			student1.setUserAge(12);
 		
-		Student student2 = new Student(000002,"Biology",5);
-			student2.setNames("Anakin");
-			student2.setSurnames("Skywalker");
-			student2.setAge(30);
+		Student student2 = new Student(000002,STUDENT_TWO_AREER,5);
+			student2.setUserNames(STUDENT_TWO_NAME);
+			student2.setUserLastName(STUDENT_TWO_LASTNAME);
+			student2.setUserAge(30);
 		
-		Teacher teacher = new Teacher("Structures", "Full time");
-			teacher.setNames("Obi-Wan");
-			teacher.setSurnames("Kenobi");
-			teacher.setAge(50);
+		Teacher teacher = new Teacher(TEACHER_ONE_COURSE, TEACHER_ONE_TIME);
+			teacher.setUserNames(TEACHER_ONE_NAME);
+			teacher.setUserLastName(TEACHER_ONE_SURNAME);
+			teacher.setUserAge(50);
 		
-		System.out.println(student1.getNames());
+		System.out.println(student1.getUserNames());
 		System.out.println(student1.getCareer());
 		
-		System.out.println(student2.getNames());
+		System.out.println(student2.getUserNames());
 		System.out.println(student2.getCareer());
 		
-		System.out.println(teacher.getNames());
+		System.out.println(teacher.getUserNames());
 		System.out.println(teacher.getAgreementTime());
 		
 	}
