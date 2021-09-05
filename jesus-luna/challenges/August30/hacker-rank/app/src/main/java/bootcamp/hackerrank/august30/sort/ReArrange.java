05cc46f497696fe6c2325681a6750cd9c59088dd
jesus-luna <chuy_braulio@hotmail.com>
2021-09-04 23:13:33 -0500

    Sort List Students

--- jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/hackerrank/august30/sort/ReArrange.java
+++ jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/hackerrank/august30/sort/ReArrange.java
@@ -1,6 +1,8 @@
 package bootcamp.hackerrank.august30.sort;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.InputMismatchException;
 import java.util.List;
 
@@ -17,13 +19,30 @@ public class ReArrange extends Solution {
 	private static final String CGPA_INPUT_INVALID = "The expected value is a number between 0.00 - 4.00 and will contain, at most, 2 digits after the decimal point.";
 	private static final String NAME_INPUT_INSTRUCTIONS = "Give the name of the student : ";
 	private static final String NAME_INPUT_INVALID = "The expected value is a string with a length between 5 - 30";
+	private static final String BEFORE_SORT = "List before being sortd";
+	private static final String AFTER_SORT = "List after being sorted by cgpa descendent, name, id";
 
 	@Override
 	public void solve() {
-		createStudentsList();
+		List<Student> studentsList = createStudentsList();
+				
+		System.out.println(ReArrange.BEFORE_SORT);
+		studentsList.forEach(student->System.out.println(student.toString()));
+		
+		System.out.println(ReArrange.AFTER_SORT);
+    Comparator<Student> comparatorByCgpa = (former,ladder) -> Double.compare(former.getCgpa(), ladder.getCgpa());
+    Comparator<Student> comparatorByName = (former,ladder) -> former.getName().compareTo(ladder.getName());
+    Comparator<Student> comparatorById = (former,ladder) -> Integer.compare(former.getId(), former.getId());
+    studentsList.sort(
+    		Comparator
+    			.nullsFirst(comparatorByCgpa.reversed())
+    			.thenComparing(comparatorByName)
+    			.thenComparing(comparatorById)
+    );
+		studentsList.forEach(student->System.out.println(student.toString()));
 	}
 
-	private void createStudentsList() {
+	private List<Student> createStudentsList() {
 		int numberOfStudents;
 		int idStudent;
 		double cgpa;
@@ -36,7 +55,7 @@ public class ReArrange extends Solution {
 			name = readName();
 			listOfStudents.add(new Student(idStudent, name, cgpa));
 		}
-		listOfStudents.forEach(s->System.out.println(s.toString()));
+		return listOfStudents;
 	}
 	
 	private int readNumberOfStudentsIteration() {
@@ -76,8 +95,10 @@ public class ReArrange extends Solution {
 	private double readCgpa() {
 		double cgpa;
 		cgpa = readDoubleInput(ReArrange.CGPA_INPUT_INSTRUCTIONS);
-		boolean cgpaFormat = String.valueOf(cgpa).matches(ReArrange.CGPA_FORMAT_INPUT);
-		if(cgpa < 0 || cgpa > 4.00 | !cgpaFormat) {
+		DecimalFormat formateador = new DecimalFormat("0.00");
+		String formatCgpa = formateador.format(cgpa);
+		boolean cgpaFormat = formatCgpa.matches(ReArrange.CGPA_FORMAT_INPUT);
+		if(cgpa < 0 || cgpa > 4.00 || !cgpaFormat) {
 			System.out.println(ReArrange.CGPA_INPUT_INVALID);
 			cgpa = readCgpa();
 		}
@@ -86,7 +107,7 @@ public class ReArrange extends Solution {
 	
 	private String readName() {
 		String name;
-		System.out.println(ReArrange.NAME_INPUT_INSTRUCTIONS);
+		System.out.print(ReArrange.NAME_INPUT_INSTRUCTIONS);
 		Solution.scanner.nextLine();
 		name = Solution.scanner.nextLine().toLowerCase();
 		if(name.length() < 5 || name.length() > 30) {
