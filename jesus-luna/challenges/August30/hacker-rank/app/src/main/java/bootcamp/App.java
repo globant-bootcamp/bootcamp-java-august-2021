c5dc038ab6eb8001ee5c7ec11a083f3fb7dd79b9
jesus-luna <chuy_braulio@hotmail.com>
2021-09-04 23:15:55 -0500

    adding ReArrange solution

--- jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/App.java
+++ jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/App.java
@@ -5,6 +5,7 @@ package bootcamp;
 
 import bootcamp.hackerrank.Solution;
 import bootcamp.hackerrank.august30.ramsonnote.RansomNote;
+import bootcamp.hackerrank.august30.sort.ReArrange;
 
 public class App {
     public static void main(String[] args) {
@@ -12,11 +13,16 @@ public class App {
     }
     
     public void testHackerRank() {
-    	Solution solution;
-  		
-  		solution = new RansomNote();
-  		solution.solve();
-  		
+    	Solution solution = null;
+    	hackerRankAugust30(solution);
   		Solution.closeScanner();
+    }
+    
+    private void hackerRankAugust30(Solution solution) {
+    	solution = new RansomNote();
+  		solution.solve();
+    	
+    	solution = new ReArrange();
+    	solution.solve();
     }
 }
