3a285db4111c641b25c5d44e0661b1e107bfe189
jesus-luna <chuy_braulio@hotmail.com>
2021-09-06 01:25:52 -0500

    Format?

--- jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/App.java
+++ jesus-luna/challenges/August30/hacker-rank/app/src/main/java/bootcamp/App.java
@@ -9,35 +9,33 @@ import bootcamp.hackerrank.august30.ramsonnote.RansomNote;
 import bootcamp.hackerrank.august30.sort.ReArrange;
 
 public class App {
-    public static void main(String[] args) {
-        new App().testHackerRank();
-    }
-    
-    public void testHackerRank() {
-    	Solution solution = null;
-    	hackerRankAugust30(solution);
-  		Solution.closeScanner();
-    }
-    
-    private void hackerRankAugust30(Solution solution) {
-    	
-    	solution = new RansomNote();
-  		solution.solve();
-    	
-    	solution = new ReArrange();
-    	solution.solve();
-    	
-    	solution = new UniquePairs();
-    	solution.solve();
-    	
-    	
-    	solution = new PhoneBook();
-    	solution.solve();
-    	
-    	solution = new QuerieList();
-    	solution.solve();
-    	
-    	solution = new QuerieArrayList();
-    	solution.solve();
-    }
+	public static void main(String[] args) {
+		new App().testHackerRank();
+	}
+
+	public void testHackerRank() {
+		Solution solution = null;
+		hackerRankAugust30(solution);
+		Solution.closeScanner();
+	}
+
+	private void hackerRankAugust30(Solution solution) {
+		solution = new RansomNote();
+		solution.solve();
+
+		solution = new ReArrange();
+		solution.solve();
+
+		solution = new UniquePairs();
+		solution.solve();
+
+		solution = new PhoneBook();
+		solution.solve();
+
+		solution = new QuerieList();
+		solution.solve();
+
+		solution = new QuerieArrayList();
+		solution.solve();
+	}
 }
