206d6842ce21aa191b547f5721ea7562693752c4
Jose <josevargas@MacBook-Air-de-Jose.local>
2021-09-06 02:05:04 -0500

    Springboot HR

--- VargasMartinezJose/SpringProject/src/main/java/com/globant/springproject/controller/HackerRank.java
+++ VargasMartinezJose/SpringProject/src/main/java/com/globant/springproject/controller/HackerRank.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class HackerRank {
     @Autowired
