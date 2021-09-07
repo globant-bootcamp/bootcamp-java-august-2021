2e72f7a6f44264225dc12e84e45fe2564e9fb374
Claudia Hernández <41920826+ClaudiaHG23@users.noreply.github.com>
2021-09-07 12:27:16 -0500

    Adding attendance from August 30th and 31st and hackerrank exercises from August 30th (#59)
    * Agregando asistencia y challenges 25 ago
    
    * making modifications on PR comments
    
    * Rename IfElse.java to ifElse.java
    
    * Rename StdinAndStdoutII.java to stdinAndStdoutII.java
    
    * Rename StdinAndStdoutI.java to stdinAndStdoutI.java
    
    * adding the package to each class
    
    * Refactoring folders
    
    * adding attendance from August 26
    
    * adding challenge august 26
    
    * adding hackerrank exercises
    
    * ÂSolving PR observations
    
    * Adding attendance from August 30
    
    * Adding hackerrank exercises from August 30
    
    * Adding attendance from August 31
    
    * adding camelcase in variables
    
    * Changing interface instead of implementation
    
    * Missing interface change instead of implementation
    
    Co-authored-by: Claudia Hernandez Garcia <claudia.hernandezgarcia01@gmail.com>

---
+++ ClaudiaHernandezGarcia/30Aug/HackerRank/hashTablesRansomNote/Solution.java
@@ -0,0 +1,29 @@
+package hashTablesRansomNote;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(firstMultipleInput[0]);
+
+        int n = Integer.parseInt(firstMultipleInput[1]);
+
+        List<String> magazine = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .collect(toList());
+
+        List<String> note = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).collect(toList());
+
+        Result.checkMagazine(magazine, note);
+
+        bufferedReader.close();
+    }
+}
---
+++ ClaudiaHernandezGarcia/30Aug/HackerRank/javaMap/Solution.java
@@ -0,0 +1,33 @@
+package javaMap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] argh) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfEntries = scanner.nextInt();
+        Map<String, Integer> map = new HashMap<>();
+        scanner.nextLine();
+        for (int i = 0; i < numberOfEntries; i++) {
+            String name = scanner.nextLine();
+            int phone = scanner.nextInt();
+            scanner.nextLine();
+
+            map.put(name, phone);
+        }
+
+        while (scanner.hasNext()) {
+            String key = scanner.nextLine();
+
+            if (map.containsKey(key)) {
+                System.out.println(key + "=" + map.get(key));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+
+        scanner.close();
+    }
+}
