3cc548c1a7a1b9cdd9ae4c6f9cc5c78d712a3381
jesus-luna <chuy_braulio@hotmail.com>
2021-09-09 01:28:06 -0500

    Adding constant messages

--- jesus-luna/challenges/hacker-rank-spring/src/main/java/com/globant/hackerrank/util/Constants.java
+++ jesus-luna/challenges/hacker-rank-spring/src/main/java/com/globant/hackerrank/util/Constants.java
@@ -15,4 +15,9 @@ public class Constants {
 			return description;
 		}		
 	}
+	
+	public static final int ZERO = 0;
+	public static final String CONSTRAINT_STRING_RANGE_ATMOST_INVALID = "Input lenght size must be within 1 - %d letters.";
+	public static final String STRING_IS_PALINDROME = "The input %s is a palindrome";
+	public static final String STRING_IS_NOT_PALINDROME = "The input %s is not a palindrome";
 }
