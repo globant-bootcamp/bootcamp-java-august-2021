05447f4d85b3f1ffdb55abe38ab45b7993beaf13
jesus-luna <chuy_braulio@hotmail.com>
2021-09-09 00:31:13 -0500

    Adding StringService and impl

---
+++ jesus-luna/challenges/hacker-rank-spring/src/main/java/com/globant/hackerrank/service/StringService.java
@@ -0,0 +1,5 @@
+package com.globant.hackerrank.service;
+
+public interface StringService {
+	public boolean isPalindrome(String inputToDeterminePalindrome);
+}
---
+++ jesus-luna/challenges/hacker-rank-spring/src/main/java/com/globant/hackerrank/service/impl/StringServiceImpl.java
@@ -0,0 +1,13 @@
+package com.globant.hackerrank.service.impl;
+
+import com.globant.hackerrank.service.StringService;
+
+public class StringServiceImpl implements StringService {
+
+	@Override
+	public boolean isPalindrome(String inputToDeterminePalindrome) {
+		return false;
+	}
+
+
+}
