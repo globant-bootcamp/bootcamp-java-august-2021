5ae2671d4773de9dd8c676fac8ecfd55f3508c01
jesus-luna <chuy_braulio@hotmail.com>
2021-09-09 01:35:53 -0500

    Validate controller input and give a response

--- jesus-luna/challenges/hacker-rank-spring/src/main/java/com/globant/hackerrank/controller/Septiembre02.java
+++ jesus-luna/challenges/hacker-rank-spring/src/main/java/com/globant/hackerrank/controller/Septiembre02.java
@@ -1,16 +1,29 @@
 package com.globant.hackerrank.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.globant.hackerrank.service.StringService;
+import com.globant.hackerrank.util.ValidatorInputs;
 
 @RestController
 @RequestMapping("septiembre02")
 public class Septiembre02 {
+	
+	@Autowired
+	private StringService stringService;
+	
+	@Autowired
+	private ValidatorInputs validator;
 
 	@GetMapping(value = "reverse")
-	public String getMethodName() {
-		return"Hello world";
+	public ResponseEntity<Boolean> getMethodName(@RequestParam(name = "input",defaultValue = "") String inputToDeterminePalindrome) {
+		validator.validAtMostNLetters(50, inputToDeterminePalindrome);
+		return ResponseEntity.ok().body(stringService.isPalindrome(inputToDeterminePalindrome));
 	}
 
 }
