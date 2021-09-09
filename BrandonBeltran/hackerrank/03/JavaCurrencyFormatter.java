29a2c1afed3fd30191f5d06b7e0483763632344c
Brandon <brandon.abr@hotmail.com>
2021-09-08 20:58:24 -0500

    borrado el proyecto de intellij que estaba sobrando (y recuperados archivos que se perdieron por ahí en el camino)

---
+++ BrandonBeltran/hackerrank/03/JavaCurrencyFormatter.java
@@ -0,0 +1,20 @@
+package bootcamp;
+
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        System.out.println("US: " + NumberFormat.getCurrencyInstance(new Locale("en","US")).format(payment));
+        System.out.println("India: " + NumberFormat.getCurrencyInstance(new Locale("en","IN")).format(payment));
+        System.out.println("China: " + NumberFormat.getCurrencyInstance(new Locale("zh","CN")).format(payment));
+        System.out.println("France: " + NumberFormat.getCurrencyInstance(new Locale("fr","FR")).format(payment));
+    }
+}
