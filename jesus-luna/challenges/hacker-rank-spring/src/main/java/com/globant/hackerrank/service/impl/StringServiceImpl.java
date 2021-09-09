86e0b4075d73d2f08f8567ec46e6ebd3d2b549f5
jesus-luna <chuy_braulio@hotmail.com>
2021-09-09 01:35:17 -0500

    adding logic to palindrome issue

--- jesus-luna/challenges/hacker-rank-spring/src/main/java/com/globant/hackerrank/service/impl/StringServiceImpl.java
+++ jesus-luna/challenges/hacker-rank-spring/src/main/java/com/globant/hackerrank/service/impl/StringServiceImpl.java
@@ -1,13 +1,22 @@
 package com.globant.hackerrank.service.impl;
 
-import com.globant.hackerrank.service.StringService;
+import org.springframework.stereotype.Service;
 
+import com.globant.hackerrank.service.StringService;
+import com.globant.hackerrank.util.Constants;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class StringServiceImpl implements StringService {
 
 	@Override
 	public boolean isPalindrome(String inputToDeterminePalindrome) {
-		return false;
-	}
+		String intpuLowerCase = inputToDeterminePalindrome.toLowerCase();
+		String inputReversed = new StringBuffer(intpuLowerCase).reverse().toString();
+		return intpuLowerCase.equalsIgnoreCase(inputReversed);
+	} 
 
 
 }
