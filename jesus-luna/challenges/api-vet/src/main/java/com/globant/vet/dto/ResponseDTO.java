efcb4ebf6fe539bfc74d0df3b5c8766b1f5f3e44
jesus-luna <chuy_braulio@hotmail.com>
2021-09-11 20:47:36 -0500

    Adding DTO response

---
+++ jesus-luna/challenges/api-vet/src/main/java/com/globant/vet/dto/ResponseDTO.java
@@ -0,0 +1,19 @@
+package com.globant.vet.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.globant.vet.util.constants.ResponseConstants;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(value = Include.NON_NULL)
+public class ResponseDTO<T> {
+	private ResponseConstants status;
+	private String message;
+	private T content;
+}
