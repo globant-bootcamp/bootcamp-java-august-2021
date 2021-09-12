3b1093780f33478c69ad29d8df9d70ef19c34e26
jesus-luna <chuy_braulio@hotmail.com>
2021-09-12 17:48:07 -0500

    Adding BootstrapExeption

--- jesus-luna/challenges/api-vet/src/main/java/com/globant/vet/exception/BootstrapException.java
+++ jesus-luna/challenges/api-vet/src/main/java/com/globant/vet/exception/BootstrapException.java
@@ -1,6 +1,7 @@
 package com.globant.vet.exception;
 
 public class BootstrapException extends RuntimeException {
+	
 	private static final long serialVersionUID = 1L;
 
 	public BootstrapException(String message) {
