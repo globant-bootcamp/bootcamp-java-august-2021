6c57fe2a2f6679e35861afcd0bafdc2cf7ae0c62
jesus-luna <chuy_braulio@hotmail.com>
2021-09-12 21:32:52 -0500

    Adding General utlity

---
+++ jesus-luna/challenges/api-vet/src/main/java/com/globant/vet/util/GeneralUtil.java
@@ -0,0 +1,23 @@
+package com.globant.vet.util;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+import com.globant.vet.dto.PetInfo;
+import com.globant.vet.model.Pet;
+
+@Component
+public class GeneralUtil {
+	public Pet overridePetWithPetInfo(Pet pet, PetInfo petInfo) {
+		int age = (pet.getAge() != petInfo.getAge()) ? petInfo.getAge() : pet.getAge();
+		LocalDateTime meeting = (pet.getMeeting() != petInfo.getMeeting()) ? petInfo.getMeeting() : pet.getMeeting();
+		String name = (pet.getName().equalsIgnoreCase(petInfo.getName())) ? pet.getName() : petInfo.getName();
+		String type = (pet.getType().equalsIgnoreCase(petInfo.getType())) ? pet.getType() : petInfo.getType();
+		pet.setAge(age);
+		pet.setMeeting(meeting);
+		pet.setName(name);
+		pet.setType(type);
+		return pet;
+	}
+}
