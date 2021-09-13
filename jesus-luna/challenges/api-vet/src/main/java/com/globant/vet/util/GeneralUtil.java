6c95a0fc277c60d6c6e8b1f47a4241a4f257afe3
jesus-luna <chuy_braulio@hotmail.com>
2021-09-13 00:14:57 -0500

    Adding a method to update customer with given info

--- jesus-luna/challenges/api-vet/src/main/java/com/globant/vet/util/GeneralUtil.java
+++ jesus-luna/challenges/api-vet/src/main/java/com/globant/vet/util/GeneralUtil.java
@@ -4,7 +4,9 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Component;
 
+import com.globant.vet.dto.CustomerInfo;
 import com.globant.vet.dto.PetInfo;
+import com.globant.vet.model.Customer;
 import com.globant.vet.model.Pet;
 
 @Component
@@ -19,5 +21,11 @@ public class GeneralUtil {
 		pet.setName(name);
 		pet.setType(type);
 		return pet;
+	}
+	
+	public Customer overrideCustomerWithCustomerInfo(Customer customer, CustomerInfo customerInfo) {
+		String name = (customer.getName().equalsIgnoreCase(customerInfo.getName())) ? customer.getName() : customerInfo.getName();
+		customer.setName(name);
+		return customer;
 	}
 }
