f80cf0dd5b2d7f288546d22e53ec1b88c70d948f
jesus-luna <chuy_braulio@hotmail.com>
2021-09-13 00:16:22 -0500

    Adding constatns used in operation update customer

--- jesus-luna/challenges/api-vet/src/main/java/com/globant/vet/util/constants/Constants.java
+++ jesus-luna/challenges/api-vet/src/main/java/com/globant/vet/util/constants/Constants.java
@@ -17,4 +17,5 @@ public class Constants {
 	public static final String CUSTOMER_FOUND_WITH_ID = "Customer corresponding to id: %d";
 	public static final String CUSTOMER_NOT_FOUND = "None customer have been found with id: %d";
 	public static final String CUSTOMER_CREATED_WITH_ID = "Customer created with id: %d";
+	public static final String CUSTOMER_UPDATE = "Customer was updated with the provided info";
 }
