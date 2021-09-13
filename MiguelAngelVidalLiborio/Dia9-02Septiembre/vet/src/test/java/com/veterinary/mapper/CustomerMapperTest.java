d2a4318bc621795286b770836ca7adfc695f43df
miguel angel <mikeangelvl1097@gmail.com>
2021-09-12 20:16:07 -0500

    Testing Controller, mapper and service. Mockito

---
+++ MiguelAngelVidalLiborio/Dia9-02Septiembre/vet/src/test/java/com/veterinary/mapper/CustomerMapperTest.java
@@ -0,0 +1,68 @@
+package com.veterinary.mapper;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.veterinary.dto.CustomerDTO;
+import com.veterinary.entity.Customer;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerMapperTest {
+
+	@Mock
+	private CustomerMapper customerMapper;
+
+	@Test
+	public void customerMapperTest_WhenCustomerEntityToCustomerDTO_Ok() {
+
+		Customer customerEntity = new Customer();
+		customerEntity.setId(1L);
+		customerEntity.setFullName("Leo Messi");
+		customerEntity.setAddress("Paris");
+		customerEntity.setPayment(800);
+
+		CustomerDTO customerDTO = new CustomerDTO();
+		customerDTO.setId(1L);
+		customerDTO.setFullName("Leo Messi");
+		customerDTO.setAddress("Paris");
+		customerDTO.setPayment(800);
+
+		Mockito.when(customerMapper.entityToCustomerDTO(customerEntity)).thenReturn(customerDTO);
+
+		CustomerDTO response = customerMapper.entityToCustomerDTO(customerEntity);
+
+		assertNotNull(response);
+
+		Mockito.verify(customerMapper, Mockito.times(1)).entityToCustomerDTO(customerEntity);
+	}
+
+	@Test
+	public void customerMapperTest_WhenCustomerDTOToCustomerEntity_Ok() {
+
+		CustomerDTO customerDTO = new CustomerDTO();
+		customerDTO.setId(1L);
+		customerDTO.setFullName("Leo Messi");
+		customerDTO.setAddress("Paris");
+		customerDTO.setPayment(800);
+
+		Customer customerEntity = new Customer();
+		customerEntity.setId(1L);
+		customerEntity.setFullName("Leo Messi");
+		customerEntity.setAddress("Paris");
+		customerEntity.setPayment(800);
+
+		Mockito.when(customerMapper.customerDTOToEntity(customerDTO)).thenReturn(customerEntity);
+
+		Customer response = customerMapper.customerDTOToEntity(customerDTO);
+
+		assertNotNull(response);
+
+		Mockito.verify(customerMapper, Mockito.times(1)).customerDTOToEntity(customerDTO);
+	}
+
+}
---
+++ MiguelAngelVidalLiborio/Dia9-02Septiembre/vet/src/test/java/com/veterinary/serviceimp/AnimalServiceImpTest.java
@@ -0,0 +1,188 @@
+package com.veterinary.serviceimp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.veterinary.dto.AnimalDTO;
+import com.veterinary.entity.Animal;
+import com.veterinary.mapper.AnimalMapper;
+import com.veterinary.repository.AnimalRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AnimalServiceImpTest {
+
+	@InjectMocks
+	private AnimalServiceImp animalService;
+
+	@Mock
+	private AnimalMapper animalMapper;
+
+	@Mock
+	private AnimalRepository animalRepository;
+
+	@Test
+	public void getAllPetsTest_WhenWeGetAllPets_Ok() {
+
+		Animal animalResponse = new Animal();
+		animalResponse.setId(1L);
+		animalResponse.setName("firulais");
+		animalResponse.setType("cat");
+		animalResponse.setAge(2);
+
+		List<Animal> listOfPets = new ArrayList<Animal>();
+		listOfPets.add(animalResponse);
+
+		when(animalRepository.findAll()).thenReturn(listOfPets);
+
+		List<Animal> response = animalService.getAllPets();
+
+		assertNotNull(response);
+
+	}
+
+	@Test
+	public void savePetTest_WhenWeSaveAPet_Ok() {
+		AnimalDTO animalDTORequest = new AnimalDTO();
+		animalDTORequest.setName("firulais");
+		animalDTORequest.setType("cat");
+		animalDTORequest.setAge(2);
+
+		AnimalDTO animalDTOResponse = new AnimalDTO();
+		animalDTOResponse.setId(1L);
+		animalDTOResponse.setName("firulais");
+		animalDTOResponse.setType("cat");
+		animalDTOResponse.setAge(2);
+
+		Animal animalRequest = new Animal();
+		animalRequest.setName("firulais");
+		animalRequest.setType("cat");
+		animalRequest.setAge(2);
+
+		Animal animalResponse = new Animal();
+		animalResponse.setId(1L);
+		animalResponse.setName("firulais");
+		animalResponse.setType("cat");
+		animalResponse.setAge(2);
+
+		when(animalMapper.animalDTOToEntity(Mockito.any(AnimalDTO.class))).thenReturn(animalRequest);
+		when(animalRepository.save(Mockito.any(Animal.class))).thenReturn(animalResponse);
+		when(animalMapper.entityToAnimalDTO(Mockito.any(Animal.class))).thenReturn(animalDTOResponse);
+
+		AnimalDTO response = animalService.savePet(animalDTORequest);
+
+		assertNotNull(response);
+		assertEquals(response, animalDTOResponse);
+		assertEquals(response.getName(), animalDTOResponse.getName());
+
+		Mockito.verify(animalMapper, times(1)).animalDTOToEntity(Mockito.any(AnimalDTO.class));
+		Mockito.verify(animalRepository, times(1)).save(Mockito.any(Animal.class));
+		Mockito.verify(animalMapper, times(1)).entityToAnimalDTO(Mockito.any(Animal.class));
+
+	}
+
+	@Test
+	public void getPetByIdTest_WhenWeGetAPet_Ok() {
+
+		long idMustBeNumber = 1L;
+
+		AnimalDTO animalDTOResponse = new AnimalDTO();
+		animalDTOResponse.setId(1L);
+		animalDTOResponse.setName("firulais");
+		animalDTOResponse.setType("cat");
+		animalDTOResponse.setAge(2);
+
+		Animal animalResponse = new Animal();
+		animalResponse.setId(1L);
+		animalResponse.setName("firulais");
+		animalResponse.setType("cat");
+		animalResponse.setAge(2);
+
+		when(animalRepository.findById(idMustBeNumber)).thenReturn(Optional.of(animalResponse));
+		when(animalMapper.entityToAnimalDTO(Mockito.any(Animal.class))).thenReturn(animalDTOResponse);
+
+		AnimalDTO responseGet = animalService.getPetById(1L);
+
+		assertEquals(1L, idMustBeNumber);
+		assertEquals(responseGet.getName(), animalDTOResponse.getName());
+
+		Mockito.verify(animalRepository, times(1)).findById(1L);
+		Mockito.verify(animalMapper, times(1)).entityToAnimalDTO(Mockito.any(Animal.class));
+	}
+
+	@Test
+	public void updatePetByIdTest_WeUpdateAPet_Ok() {
+		long petId = 1L;
+		AnimalDTO animalDTORequest = new AnimalDTO();
+		animalDTORequest.setId(1L);
+		animalDTORequest.setName("firulais");
+		animalDTORequest.setType("cat");
+		animalDTORequest.setAge(2);
+
+		AnimalDTO animalDTOResponse = new AnimalDTO();
+		animalDTOResponse.setId(1L);
+		animalDTOResponse.setName("firulais");
+		animalDTOResponse.setType("cat");
+		animalDTOResponse.setAge(2);
+
+		Animal animalRequest = new Animal();
+		animalRequest.setName("firulais");
+		animalRequest.setType("cat");
+		animalRequest.setAge(2);
+
+		Animal animalResponse = new Animal();
+		animalResponse.setId(1L);
+		animalResponse.setName("firulais");
+		animalResponse.setType("cat");
+		animalResponse.setAge(2);
+
+		when(animalRepository.findById(petId)).thenReturn(Optional.of(animalResponse));
+		when(animalMapper.animalDTOToEntity(Mockito.any(AnimalDTO.class))).thenReturn(animalRequest);
+		when(animalRepository.save(Mockito.any(Animal.class))).thenReturn(animalResponse);
+		when(animalMapper.entityToAnimalDTO(Mockito.any(Animal.class))).thenReturn(animalDTOResponse);
+
+		AnimalDTO response = animalService.updatePet(animalDTORequest);
+
+		assertNotNull(response);
+		assertEquals(response.getId(), animalDTOResponse.getId());
+
+		Mockito.verify(animalMapper, times(1)).animalDTOToEntity(Mockito.any(AnimalDTO.class));
+		Mockito.verify(animalRepository, times(1)).save(Mockito.any(Animal.class));
+
+	}
+
+	public void deletePetByIdTest_WeDeletedAPet_Ok() {
+		long petId = 1L;
+
+		Animal animalRequest = new Animal();
+		animalRequest.setName("firulais");
+		animalRequest.setType("cat");
+		animalRequest.setAge(2);
+
+		Animal animalResponse = new Animal();
+		animalResponse.setId(1L);
+		animalResponse.setName("firulais");
+		animalResponse.setType("cat");
+		animalResponse.setAge(2);
+
+		when(animalRepository.findById(petId)).thenReturn(Optional.of(animalResponse));
+		when(animalMapper.animalDTOToEntity(Mockito.any(AnimalDTO.class))).thenReturn(animalRequest);
+
+		animalRepository.delete(animalRequest);
+
+		Mockito.verify(animalMapper, times(1)).animalDTOToEntity(Mockito.any(AnimalDTO.class));
+	}
+
+}
