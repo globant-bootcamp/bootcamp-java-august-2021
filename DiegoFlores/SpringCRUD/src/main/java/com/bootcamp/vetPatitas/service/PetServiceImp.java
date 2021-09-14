7a02319fc8c244b3a9a58a76e63768bacad2aebc
Diego Flores Alemán <55201763+Dfloresa@users.noreply.github.com>
2021-09-13 23:31:35 -0500

    Update PetServiceImp.java
    Package name updated

--- DiegoFlores/SpringCRUD/src/main/java/com/bootcamp/vetPatitas/service/PetServiceImp.java
+++ DiegoFlores/SpringCRUD/src/main/java/com/bootcamp/vetPatitas/service/PetServiceImp.java
@@ -1,18 +1,18 @@
-package com.bootcamp.vetPatitas.service;
+package com.bootcamp.vetpatitas.service;
 
-import static com.bootcamp.vetPatitas.utils.Constants.PET_INVALID_PHONE;
-import static com.bootcamp.vetPatitas.utils.Constants.PET_NULL_NAME;
-import static com.bootcamp.vetPatitas.utils.Constants.PET_NULL_TYPE;
-import static com.bootcamp.vetPatitas.utils.Constants.PET_NULL_AGE;
-import static com.bootcamp.vetPatitas.utils.Constants.PET_NULL_OWNER;
+import static com.bootcamp.vetpatitas.utils.Constants.PET_INVALID_PHONE;
+import static com.bootcamp.vetpatitas.utils.Constants.PET_NULL_NAME;
+import static com.bootcamp.vetpatitas.utils.Constants.PET_NULL_TYPE;
+import static com.bootcamp.vetpatitas.utils.Constants.PET_NULL_AGE;
+import static com.bootcamp.vetpatitas.utils.Constants.PET_NULL_OWNER;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bootcamp.vetPatitas.dao.PetDAO;
-import com.bootcamp.vetPatitas.dto.PetDTO;
-import com.bootcamp.vetPatitas.exception.InvalidDataException;
-import com.bootcamp.vetPatitas.mapper.PetMapper;
+import com.bootcamp.vetpatitas.dao.PetDAO;
+import com.bootcamp.vetpatitas.dto.PetDTO;
+import com.bootcamp.vetpatitas.exception.InvalidDataException;
+import com.bootcamp.vetpatitas.mapper.PetMapper;
 
 
 @Service
