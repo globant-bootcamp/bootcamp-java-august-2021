09bec4b9be1587a3b2d9590358a618b75d147238
Diego Flores Alemán <55201763+Dfloresa@users.noreply.github.com>
2021-09-14 23:10:55 -0500

    Update PetController.java
    Endpoints rename, verbs changed

--- DiegoFlores/SpringCRUD/src/main/java/com/bootcamp/vetPatitas/userController/PetController.java
+++ DiegoFlores/SpringCRUD/src/main/java/com/bootcamp/vetPatitas/userController/PetController.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,32 +27,32 @@ import com.bootcamp.vetpatitas.utils.Constants.ResponseConstants;
 public class PetController {
 
 	@Autowired
-	PetService petService;
+	private PetService petService;
 	
-	@PutMapping(path = "/savePet",produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(path = "/",produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ResponseDTO<PetDTO>> savePet(@RequestBody PetDTO pet) {	
 			
 		ResponseDTO responseDTO  = new ResponseDTO(ResponseConstants.SUCCESS, PET_ADDED_SUCCESSFULLY , petService.addPet(pet));		
 		return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
 	}
 	
-	@GetMapping(path = "/getpet/{petId}",produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path = "/{petId}",produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ResponseDTO<PetDTO>> getPet(@PathVariable Long petId) {		
 		ResponseDTO responseDTO  = new ResponseDTO(ResponseConstants.SUCCESS, PET_FOUNDED_SUCCESSFULLY , petService.getPet(petId));
 		
 		return new ResponseEntity<>(responseDTO, HttpStatus.FOUND);
 	}
 	
-	@PutMapping(path = "/updatePet",produces = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(path = "/",produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ResponseDTO<PetDTO>> updatePet(@RequestBody PetDTO pet) {		
 		ResponseDTO responseDTO  = new ResponseDTO(ResponseConstants.SUCCESS, PET_UPDATED_SUCCESSFULLY , petService.updatePet(pet));
 		
 		return new ResponseEntity<>(responseDTO, HttpStatus.OK);
 	}
 	
-	@PutMapping(path = "/deletePet",produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseDTO<PetDTO>> deletePet(@RequestBody PetDTO pet) {		
-		ResponseDTO responseDTO  = new ResponseDTO(ResponseConstants.SUCCESS, PET_DELETED_SUCCESSFULLY , petService.deletePet(pet));
+	@PutMapping(path = "/{petId}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseDTO<PetDTO>> deletePet(@PathVariable Long petId) {		
+		ResponseDTO responseDTO  = new ResponseDTO(ResponseConstants.SUCCESS, PET_DELETED_SUCCESSFULLY , petService.deletePet(petId));
 		
 		return new ResponseEntity<>(responseDTO, HttpStatus.OK);
 	}
