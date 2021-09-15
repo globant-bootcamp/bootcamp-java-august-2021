2d8f221a0606c6473ab0f9fa7e8cfdad84166a01
jesus-luna <chuy_braulio@hotmail.com>
2021-09-15 00:30:14 -0500

    Testing Util package

--- jesus-luna/challenges/api-vet/src/test/java/com/globant/vet/utilities/ConstantsTests.java
+++ jesus-luna/challenges/api-vet/src/test/java/com/globant/vet/utilities/ConstantsTests.java
@@ -8,23 +8,25 @@ public class ConstantsTests {
 	public static final int ID_PET_2 = 11;
 	public static final int ID_PET_4 = 12;
 	public static final int ID_PET_3 = 13;
-	
-	
 	public static final int ID_PET_NOT_FOUND = 100;
 	public static final int ID_CUSTOMER_NOT_FOUND = 200;
+	public static final int ID_CUSTOMER = 20;
+	public static final int ID_CUSTOMER_2 = 30;
+	
 	public static final String NAME_PET = "Riley";
 	public static final String TYPE_PET = "Canine";
 	public static final int AGE_PET = 1;
 	public static final LocalDateTime MEETING_PET = LocalDateTime.of(2021, Month.DECEMBER, 29, 19, 30, 00);
+	public static final String NAME_PET_2 = "Zeus";
+	public static final String TYPE_PET_2 = "Canine";
+	public static final int AGE_PET_2 = 5;
+	public static final LocalDateTime MEETING_PET_2 = LocalDateTime.of(2021, Month.AUGUST, 29, 19, 30, 00);
 	
-	public static final int ID_CUSTOMER = 20;
 	public static final String NAME_CUSTOMER = "Fulanito";
-	public static final int ID_CUSTOMER_2 = 30;
 	public static final String NAME_CUSTOMER_2 = "Menganito";
 	
 	public static final String PET_NOT_FOUND = "None pet have been found with id: %d";
 	public static final String PET_DELETED = "Pet was deleted";
-	
 	public static final String CUSTOMER_NOT_FOUND = "None customer have been found with id: %d";
 	public static final String CUSTOMER_DELETED = "Customer was deleted";
 	
